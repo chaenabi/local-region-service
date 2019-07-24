ba69e2b91040aec12162206c306ece2e46af0f57
charlie <find1086@gmail.com>
2019-07-25 01:28:43 +0900

    test

--- lib/map.dart
+++ lib/map.dart
@@ -34,17 +34,15 @@ class _MapPageState extends State<MapPage> {
       //color: Colors.blue,
       child: Row(
         children: <Widget>[
-         
           Expanded(
             child: SideBar(territorylist),
           ),
-        ShowList(),
-        //   Container(
-        //       width: 350.0,
-        //       child: Image.network(
-        //           'https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/11203249_399567670227980_8018633873156838169_n.png?_nc_cat=104&_nc_oc=AQmQtBN12xY-QYwbZ23HilhynOM2bdFnpHH35tNGPLVPqxQ1X284jQu5i3tC9PjXpIU&_nc_ht=scontent-icn1-1.xx&oh=88da3ae9cccbedb81a571ceaf6e1d5fc&oe=5DE41A73')),
-        //  
-       
+          ShowList(),
+          //   Container(
+          //       width: 350.0,
+          //       child: Image.network(
+          //           'https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/11203249_399567670227980_8018633873156838169_n.png?_nc_cat=104&_nc_oc=AQmQtBN12xY-QYwbZ23HilhynOM2bdFnpHH35tNGPLVPqxQ1X284jQu5i3tC9PjXpIU&_nc_ht=scontent-icn1-1.xx&oh=88da3ae9cccbedb81a571ceaf6e1d5fc&oe=5DE41A73')),
+          //
         ],
       ),
     );
